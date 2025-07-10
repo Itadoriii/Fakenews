6d4c9275ccf4cf391452090f6712c76f382d19dd
sebas <scastroc@utem.cl>
2025-07-09 22:44:16 -0400

    Final

--- fakenews_project/prediccion/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.predict_noticia_view, name='predecir'),  # ← raíz "/"
-    path('predict/', views.predict_noticia_view, name='predict_noticia'),
-]
